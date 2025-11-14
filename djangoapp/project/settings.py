f0cde7c782da915bfeedaf9b37c795a792898228
lucasfm10 <lucasfm.py@gmail.com>
2025-11-14 00:17:35 -0300

    Projeto configurado com sucesso para Django e Docker

--- djangoapp/project/settings.py
+++ djangoapp/project/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-DATA_DIR = BASE_DIR.parent / 'data' / 'web'
 
 
 # Quick-start development settings - unsuitable for production
@@ -124,11 +123,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 # /data/web/static
-STATIC_ROOT = DATA_DIR / 'static'
+STATIC_ROOT = Path('/data/web/static')
 
 MEDIA_URL = '/media/'
 # /data/web/media
-MEDIA_ROOT = DATA_DIR / 'media'
+MEDIA_ROOT = Path('/data/web/media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
